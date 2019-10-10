9539db4f32c8f6196fba93ec201638d9c9afbfa5
Dustinyschild <dyelland@unitedfiregroup.com>
2019-10-10 18:17:20 -0500

    set up server routing architecture

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
 const express = require('express');
 const next = require('next');
+const apiRoutes = require("./api");
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+
 app.prepare()
   .then(() => {
     const server = express();
 
-    server.get("/api*", (req, res) => {
-      console.log(req);
-      return res.send("API routes here");
-    });
+    server.use("/api", apiRoutes);
 
     server.get("*", (req, res) => {
       return handle(req, res);
     });
-
-  //   server.get('/p/:id', (req, res) => {
-  //     const actualPage = '/post'
-  //     const queryParams = { id: req.params.id }
-  //     app.render(req, res, actualPage, queryParams)
-  // })
 
     server.listen(3000, err => {
       if (err) throw err;
