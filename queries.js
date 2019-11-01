61acc9c2ac1d4da71a18ffd3ad9db19238534e5b
Dustinyschild <dyelland@unitedfiregroup.com>
2019-10-31 22:24:50 -0500

    set up password hasher

--- queries.js
+++ queries.js
@@ -7,21 +7,20 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
-const getUsers = next => pool.query("SELECT * FROM users ORDER BY id ASC;", (error, results) => {
+const getUsers = callback => pool.query("SELECT * FROM users ORDER BY id ASC;", (error, results) => {
     if (error) return error;
 
-    console.log("before:", results.rows)
-    return next(results.rows);
+    return callback(results.rows);
   });
 
-const createUser = (config, next) => {
-  const { username, email, password, role } = config
+const createUser = (config, callback) => {
+  const { username, email, password, role } = config;
   pool.query("INSERT INTO users (username, email, password, role) VALUES ($1, $2, $3, $4)",
   [username, email, password, role],
   (error, results) => {
     if (error) throw new Error(error);
 
-    next(results);
+    callback(results);
   });
 };
 
--- scripts/seed_user.js
+++ scripts/seed_user.js
@@ -1,20 +1,29 @@
 require("dotenv").config();
+const bcrypt = require("bcrypt");
 const { getUsers, createUser } = require("../queries");
 
 const adminUser = {
   username: "user",
   email: "example@email.com",
-  password: "password",
+  password: bcrypt.hashSync("password", 10, (error, hash) => {
+    if (error) {
+      throw new Error(error)
+    }
+
+    return hash;
+  }),
   role: "admin"
 }
 
 getUsers((results) => {
   const userExists = results.filter(user => {
     return adminUser.username === user.username;
-  });
+  }).length > 0;
+
   if (!userExists) {
-    createUser(adminUser, res => {
+    return createUser(adminUser, res => {
       console.log("User created:\n", res);
+      process.exit(0);
     });
   } else {
     throw new Error("User already exists.");
